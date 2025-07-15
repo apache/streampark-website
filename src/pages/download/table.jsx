9c190e8bdeb01254d9e8143409e5f740d93abeea
benjobs <benjobs@apache.org>
2025-07-15 11:39:15 +0800

    [Improve] download link issue fixed (#436)

--- src/pages/download/table.jsx
+++ src/pages/download/table.jsx
@@ -9,78 +9,80 @@ export default function (props) {
   const tableData = props.dataSource || []
   const latest = props.latest || false
 
-  const dynURL = 'https://www.apache.org/dyn/closer.lua/incubator/streampark/';
-  const archiveURL = 'https://archive.apache.org/dist/incubator/streampark/';
-  const downloadURL = 'https://downloads.apache.org/incubator/streampark/'
+  const dynURL = 'https://www.apache.org/dyn/closer.lua/streampark/';
+  const archiveIncubatorURL = 'https://archive.apache.org/dist/incubator/streampark/';
+  const archiveTLPURL = 'https://archive.apache.org/dist/streampark/';
+  const downloadIncubatorURL = 'https://downloads.apache.org/incubator/streampark/'
+  const downloadTLPURL = 'https://downloads.apache.org/streampark/'
   const releaseNoteUrl = 'https://streampark.apache.org/download/release-note/'
 
-  function getSourceLink(version) {
-    const prefix = latest ? dynURL : archiveURL
+  function getSourceLink(version, tlp) {
+    const prefix = latest ? dynURL : (tlp ? archiveTLPURL: archiveIncubatorURL);
     return prefix
         .concat(version)
         .concat('/apache-streampark-')
         .concat(version)
-        .concat('-incubating-src.tar.gz')
+        .concat(tlp ? '-src.tar.gz.asc' : '-incubating-src.tar.gz')
         .concat('?action=download')
   }
 
-  function getBinaryLink(scala, version) {
-    const prefix = latest ? dynURL : archiveURL;
+  function getBinaryLink(scala, tlp, version) {
+    const prefix = latest ? dynURL : (tlp ? archiveTLPURL: archiveIncubatorURL);
     return prefix
         .concat(version)
         .concat('/apache-streampark_')
         .concat(scala)
         .concat('-')
         .concat(version)
-        .concat('-incubating-bin.tar.gz')
+        .concat(tlp ? '-bin.tar.gz' : '-incubating-bin.tar.gz')
         .concat('?action=download')
   }
 
-  function getSourceSigs(version, suffix) {
-    const prefix = latest ? downloadURL : archiveURL;
+  function getSourceSigs(version, tlp, suffix) {
+    const prefix = latest ? (tlp ? downloadTLPURL: downloadIncubatorURL) : (tlp ? archiveTLPURL: archiveIncubatorURL);
     return prefix.concat(version)
         .concat('/apache-streampark-')
         .concat(version)
-        .concat('-incubating-src.tar.gz')
+        .concat(tlp ? '-src.tar.gz' : '-incubating-src.tar.gz')
         .concat(suffix)
   }
 
-  function getBinarySigs(scala, version, suffix) {
-    const prefix = latest ? downloadURL : archiveURL;
+  function getBinarySigs(scala, tlp, version, suffix) {
+    const prefix = latest ? (tlp ? downloadTLPURL: downloadIncubatorURL) : (tlp ? archiveTLPURL: archiveIncubatorURL);
     return prefix
         .concat(version)
         .concat('/apache-streampark_')
         .concat(scala)
         .concat('-')
         .concat(version)
-        .concat('-incubating-bin.tar.gz')
+        .concat(tlp ? '-bin.tar.gz' : '-incubating-bin.tar.gz')
         .concat(suffix)
   }
 
   const releaseData = tableData.map((release) => {
     return genRelease(release, latest)
   })
-  function genRelease({ version, date }, latest) {
+  function genRelease({ version, date, tlp }, latest) {
     return {
       latest: latest,
       version: version,
       date: date,
       releaseNotesUrl: `${releaseNoteUrl}${version}`,
       source: {
-        url: getSourceLink(version),
-        signature: getSourceSigs(version, '.asc'),
-        sha512: getSourceSigs(version, '.sha512')
+        url: getSourceLink(version, tlp),
+        signature: getSourceSigs(version, tlp, '.asc'),
+        sha512: getSourceSigs(version, tlp,'.sha512')
       },
       binary: [{
         name: 'apache-streampark_2.12-' + version + '-bin.tar.gz',
-        url: getBinaryLink('2.12', version),
-        signature: getBinarySigs('2.12', version, '.asc'),
-        sha512: getBinarySigs('2.12', version, '.sha512')
+        url: getBinaryLink('2.12', tlp, version),
+        signature: getBinarySigs('2.12', tlp, version, '.asc'),
+        sha512: getBinarySigs('2.12', tlp, version, '.sha512')
       }, {
         name: 'apache-streampark_2.11-' + version + '-bin.tar.gz',
-        url: getBinaryLink('2.11', version),
-        signature: getBinarySigs('2.11', version, '.asc'),
-        sha512: getBinarySigs('2.11', version, '.sha512')
+        url: getBinaryLink('2.11', tlp, version),
+        signature: getBinarySigs('2.11', tlp, version, '.asc'),
+        sha512: getBinarySigs('2.11', tlp, version, '.sha512')
       }]
     }
   }
